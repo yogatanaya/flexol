bc2ae91933751da72232f50146879d6a7810fbc8
Yoga Permana Tanaya <imgndsign@gmail.com>
2024-10-09 02:59:29 +0800

    fix2

--- components/Item.tsx
+++ components/Item.tsx
@@ -100,15 +100,11 @@ export const Item = ({
   };
 
   const exponentToSuperscript = (exp: number): string => {
-    const superscriptMap: { [key: number]: string } = {
-      0: "⁰", 1: "¹", 2: "²", 3: "³", 4: "⁴", 5: "⁵", 
-      6: "⁶", 7: "⁷", 8: "⁸", 9: "⁹", "-": "⁻"
-    };
-  
+    const superscriptMap: { [key: number]: string } = {};
     return exp
       .toString()
       .split("")
-      .map(char => superscriptMap[parseInt(char)] || superscriptMap[char])
+      // .map(char => superscriptMap[parseInt(char)] || superscriptMap[char])
       .join("");
   };
   
